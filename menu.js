deaac28d510c11757d69fdd5141b8bd190fe0a8d
Jeovanny <jeov94@gmail.com>
2017-11-24 18:22:32 -0600

    menu resposive no finished

---
+++ menu.js
@@ -0,0 +1,20 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main() {
+  $('.nav-bar__collapsed').click(function(){
+    $('.menu').toggle();
+      if(contador == 1) {
+        $('.header').animate({
+          height: '80'
+        });
+        contador = 0;
+      } else {
+        contador = 1;
+        $('.header').animate({
+          height: '400'
+        });
+      }
+  });
+};
